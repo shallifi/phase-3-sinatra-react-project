7fbb81a33b0e19432918993ba2fe20b4f7b0c8d5
Benjamin <shallifi@hotmail.com>
2022-06-30 23:26:26 -0400

    get routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,20 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # the get routes 
   get "/show_pieces" do 
     ShowPiece.all.to_json
   end
+
+  get "/audience_members" do
+    AudienceMember.all.to_json
+  end
+
+  get "/feedback" do
+    Feedback.all.to_json
+  end
+
+    
+
 
 end
